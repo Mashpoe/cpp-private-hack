282e2071909a0fadb9157432082a81bd465552a3
Mashpoe <mashpoesemail@gmail.com>
2020-12-28 08:07:54 -0700

    Create hack.hpp

---
+++ hack.hpp
@@ -0,0 +1,41 @@
+//
+//  Hack.hpp
+//  Hack_Private
+//
+//  Created by Mashpoe on 12/23/20.
+//
+
+#ifndef Hack_h
+#define Hack_h
+
+#include <memory>
+
+namespace {
+  // This is a *different* type in every translation
+  // unit because of the anonymous namespace.
+  struct TranslationUnitTag {};
+}
+
+// here TranslationUnitTag is just a placeholder,
+// this serves as a member function pointer equivalent to void*
+typedef void (TranslationUnitTag::*hijack_fun_t)();
+
+#define hijack_method(hijack_target, hijack_name)\
+\
+hijack_fun_t hijack_name();\
+\
+template <typename Tag,\
+typename ForbiddenFunc,\
+ForbiddenFunc forbidden_func>\
+class hijack_ ## hijack_name {\
+  \
+	friend hijack_fun_t hijack_name() {\
+		return (hijack_fun_t)forbidden_func;\
+	}\
+  \
+  \
+};\
+\
+template class hijack_ ## hijack_name<TranslationUnitTag, decltype(&hijack_target), &hijack_target>;
+
+#endif /* Hack_h */
